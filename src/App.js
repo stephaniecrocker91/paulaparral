383d82c97fd00c40bc1eb03d4f27e5f9201730ad
Stephie <stephaniecrocker91@gmail.com>
2023-09-08 18:18:13 +0000

    Create routes for navigation: home and registros

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import Landing from './components/Landing';
 import NavBar from './components/NavBar';
 import Intro from './components/Intro';
 import Footer from './components/Footer';
-
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 
 function App() {
   return (
     <>
     <NavBar />
-    <Landing />
-    <Intro />
-    <Footer />
+    <Container className={StyleSheet.Main}>
+      <Routes>
+        <Route exact="true" path="/" element= {
+        <div>
+          <Landing />
+          <Intro />
+        </div>   
+      } />
+        <Route exact="true" path="/registrosakashicos" element= {<Intro />} />
+      </Routes>    
+      <Footer />
+    </Container>
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,85 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom'
 
 function NavBar() {
-  return (
-    <Navbar expand="lg" className={`bg-body-tertiary ${styles.NavBar}`}>
-      <Container fluid>
-        <Navbar.Brand href="#" className={styles.Logo}>Paula Parral</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className={`me-auto my-2 my-lg-0 ${styles.Links}`}
-            
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1" className={styles.Link}>Home</Nav.Link>
-            <Nav.Link href="#action2" className={styles.Link}>Registros Akashicos</Nav.Link>
-            <Nav.Link href="#action3" className={styles.Link}>Acerca de la Lectora</Nav.Link>
-            <Nav.Link href="#action3" className={styles.Link}>Blog</Nav.Link>
-            <Nav.Link href="#action4" className={styles.Link}>Contacto</Nav.Link>
+    return (
+        <Navbar expand="lg"
+            className={
+                `bg-body-tertiary ${
+                    styles.NavBar
+                }`
+        }>
+            <Container fluid>
+                <NavLink exact="true" to="/">
+                    <Navbar.Brand className={
+                        styles.Logo
+                    }>Paula Parral</Navbar.Brand>
+                </NavLink>
+                <Navbar.Toggle aria-controls="navbarScroll"/>
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className={
+                            `me-auto my-2 my-lg-0 ${
+                                styles.Links
+                            }`
+                        }
 
-            
-            
-            
-          </Nav>
-          <div className="d-flex">
-          <a className={styles.Social} href="https://www.facebook.com/paula.parral.7" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook"></i></a>
-          <a className={styles.Social} href='https://www.instagram.com/paulaparral/'target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-square-instagram"></i></a>
-          <a className={styles.Social} href="mailto:paulaparral@hotmail.com"><i class="fa-solid fa-envelope"></i></a>
-          </div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+                        style={
+                            {maxHeight: '100px'}
+                        }
+                        navbarScroll>
+                        <NavLink exact="true" to="/"
+                            className={
+                                styles.Link
+                        }>Home</NavLink>
+                        <NavLink to="/registrosakashicos"
+                            className={
+                                styles.Link
+                        }>Registros Akashicos</NavLink>
+                        <NavLink href="#action3"
+                            className={
+                                styles.Link
+                        }>Acerca de la Lectora</NavLink>
+                        <NavLink href="#action3"
+                            className={
+                                styles.Link
+                        }>Blog</NavLink>
+                        <NavLink href="#action4"
+                            className={
+                                styles.Link
+                        }>Contacto</NavLink>
+
+                    </Nav>
+                    <div className="d-flex">
+                        <a className={
+                                styles.Social
+                            }
+                            href="https://www.facebook.com/paula.parral.7"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+                        <a className={
+                                styles.Social
+                            }
+                            href='https://www.instagram.com/paulaparral/'
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            <i class="fa-brands fa-square-instagram"></i>
+                        </a>
+                        <a className={
+                                styles.Social
+                            }
+                            href="mailto:paulaparral@hotmail.com">
+                            <i class="fa-solid fa-envelope"></i>
+                        </a>
+                    </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default NavBar;
